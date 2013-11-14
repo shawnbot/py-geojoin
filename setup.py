a1244eef7877c1930642e9a18ede1eae93cf0c3e
Shawn Allen <shawn@stamen.com>
2013-11-13 16:05:44 -0800

    setup.py for pypi

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import pkg_resources
+import geojoin
+
+version = geojoin.VERSION
+
+setup(name='geojoin',
+      version=version,
+      description='Merge CSV data into GeoJSON features, and more',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Text Processing :: Filters',
+          'Topic :: Utilities'
+      ],
+      author='Shawn Allen',
+      author_email='shawn@stamen.com',
+      url='http://github.com/shawnbot/py-geojoin',
+      packages=['geojoin'],
+      requires=[
+          'csv',
+          'json'
+      ],
+      scripts=[
+          'geojoin.py'
+      ],
+      download_url='https://github.com/shawnbot/py-geojoin/archive/v%s.tar.gz' % version,
+      license='BSD')
